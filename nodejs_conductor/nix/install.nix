59ccca26cf043423a95f1008fc575f603ae78bd7
David Meister <thedavidmeister@gmail.com>
2019-06-12 18:06:27 +1000

    wip on holonix

---
+++ nodejs_conductor/nix/install.nix
@@ -0,0 +1,16 @@
+{ pkgs }:
+let
+  name = "hcnjs-conductor-install";
+
+  script = pkgs.writeShellScriptBin name
+  ''
+  echo "installing node conductor"
+  hc-node-flush
+  ( \
+   cd nodejs_conductor \
+   && yarn install --ignore-scripts \
+   && RUST_SODIUM_DISABLE_PIE=1 node ./publish.js \
+  )
+  '';
+in
+script
